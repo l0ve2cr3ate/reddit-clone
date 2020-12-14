f292b1da6ad05beb878d468deb28c532b99b469e
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-14 11:08:48 +0100

    feat: Add session authentication using redis and express-session.

--- reddit-clone-server/src/index.ts
+++ reddit-clone-server/src/index.ts
@@ -1,12 +1,19 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import mikroConfig from "./mikro-orm.config";
 import express from "express";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+
+import mikroConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+require("dotenv").config();
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -14,12 +21,36 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        // 10 years
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax", // csrf -> first-party cookie
+        // cookie only works in https:
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   app.get("/", (_, res) => {
