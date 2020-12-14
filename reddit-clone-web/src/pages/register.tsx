62bce56209c21f403103021ba9b11c4f8909f4ed
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-14 20:41:14 +0100

    feat: Create register page with register form.

---
+++ reddit-clone-web/src/pages/register.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Formik, Form } from "formik";
+
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+
+const Register: React.FC = () => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                label="Password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
