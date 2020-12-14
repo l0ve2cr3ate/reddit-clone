f15e213cd0d78c8d1f0b09fd2d65ec8c41cb0d89
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-14 11:21:56 +0100

    feat: Create me query for fetching the user.

--- reddit-clone-server/src/resolvers/user.ts
+++ reddit-clone-server/src/resolvers/user.ts
@@ -5,6 +5,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -38,10 +39,19 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // user not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -85,6 +95,10 @@ export class UserResolver {
       }
       console.error("message: ", err.message);
     }
+
+    // store userId session, this will set a cookie on the user
+    // and keep them logged in
+    req.session.userId = user.id;
 
     return { user };
   }
