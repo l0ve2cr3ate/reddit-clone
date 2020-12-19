6db4fbfaf5af26e50abb76b43cd47bdfd24236b5
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-19 20:19:43 +0100

    refactor: Implement logic for upvoting post. User can only upvote/ downvote once. User can change upvote to downvote, or downvote to upvote.

--- reddit-clone-server/src/resolvers/post.ts
+++ reddit-clone-server/src/resolvers/post.ts
@@ -52,27 +52,49 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-    // await Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
+    // user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          ` update updoot
+            set value = $1
+            where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
 
-    await getConnection().query(
-      `
-    START TRANSACTION;
+        await tm.query(
+          `
+          update post 
+          set points = points + $1
+          where id = $2
+          `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      // user has never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          ` insert into updoot("userId", "postId", value)
+            values ($1,$2,$3)
+        `,
+          [userId, postId, realValue]
+        );
 
-    insert into updoot("userId", "postId", value)
-    values (${userId},${postId},${realValue});
-
-    update post 
-    set points = points + ${realValue}
-    where id = ${postId};
-
-    COMMIT;
-    `
-    );
+        await tm.query(
+          `
+          update post 
+          set points = points + $1
+          where id = $2
+          `,
+          [realValue, postId]
+        );
+      });
+    }
 
     return true;
   }
--- reddit-clone-web/src/components/UpdootSection.tsx
+++ reddit-clone-web/src/components/UpdootSection.tsx
@@ -1,21 +1,37 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { Flex, IconButton, Text } from "@chakra-ui/react";
-import { FC } from "react";
-import { PostsQuery } from "../generated/graphql";
+import { FC, useState } from "react";
+import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
 
 interface UpdootSectionProps {
-  post: PostsQuery["posts"]["posts"][0];
+  post: PostSnippetFragment;
 }
 
 const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "updoot-loading" | "downdoot-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
   return (
     <Flex direction="column" alignItems="center" justifyContent="center" mr={4}>
       <IconButton
+        onClick={async () => {
+          setLoadingState("updoot-loading");
+          await vote({ postId: post.id, value: 1 });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "updoot-loading"}
         aria-label="Upvote post"
         icon={<ChevronUpIcon size="24px" />}
       />
       <Text>{post.points}</Text>
       <IconButton
+        onClick={async () => {
+          setLoadingState("downdoot-loading");
+          await vote({ postId: post.id, value: -1 });
+          setLoadingState("not-loading");
+        }}
+        isLoading={loadingState === "downdoot-loading"}
         aria-label="Downvote post"
         icon={<ChevronDownIcon size="24px" />}
       />
