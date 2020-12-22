b8a946581bd1d01754cbdfcb758a29d7f5497115
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-22 19:30:25 +0100

    refactor: Convert updoot to apollo client + update cache after upvoting/down voting.

--- reddit-clone-web/src/components/UpdootSection.tsx
+++ reddit-clone-web/src/components/UpdootSection.tsx
@@ -1,24 +1,70 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { Flex, IconButton, Text } from "@chakra-ui/react";
 import { FC, useState } from "react";
-import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
+import gql from "graphql-tag";
+import {
+  PostSnippetFragment,
+  useVoteMutation,
+  VoteMutation,
+} from "../generated/graphql";
+import { ApolloCache } from "@apollo/client";
 
 interface UpdootSectionProps {
   post: PostSnippetFragment;
 }
 
+const updateAfterVote = (
+  value: number,
+  postId: number,
+  cache: ApolloCache<VoteMutation>
+) => {
+  const data = cache.readFragment<{
+    id: number;
+    points: number;
+    voteStatus: number | null;
+  }>({
+    id: "Post:" + postId,
+    fragment: gql`
+      fragment _ on Post {
+        id
+        points
+        voteStatus
+      }
+    `,
+  });
+
+  if (data) {
+    if (data.voteStatus === value) return;
+
+    const newPoints = data.points + (!data.voteStatus ? 1 : 2) * value;
+    cache.writeFragment({
+      id: "Post:" + postId,
+      fragment: gql`
+        fragment _ on Post {
+          points
+          voteStatus
+        }
+      `,
+      data: { points: newPoints, voteStatus: value },
+    });
+  }
+};
+
 const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
   const [loadingState, setLoadingState] = useState<
     "updoot-loading" | "downdoot-loading" | "not-loading"
   >("not-loading");
-  const [, vote] = useVoteMutation();
+  const [vote] = useVoteMutation();
   return (
     <Flex direction="column" alignItems="center" justifyContent="center" mr={4}>
       <IconButton
         onClick={async () => {
           if (post.voteStatus === 1) return;
           setLoadingState("updoot-loading");
-          await vote({ postId: post.id, value: 1 });
+          await vote({
+            variables: { postId: post.id, value: 1 },
+            update: (cache) => updateAfterVote(1, post.id, cache),
+          });
           setLoadingState("not-loading");
         }}
         colorScheme={post.voteStatus === 1 ? "green" : undefined}
@@ -31,7 +77,10 @@ const UpdootSection: FC<UpdootSectionProps> = ({ post }) => {
         onClick={async () => {
           if (post.voteStatus === -1) return;
           setLoadingState("downdoot-loading");
-          await vote({ postId: post.id, value: -1 });
+          await vote({
+            variables: { postId: post.id, value: -1 },
+            update: (cache) => updateAfterVote(-1, post.id, cache),
+          });
           setLoadingState("not-loading");
         }}
         colorScheme={post.voteStatus === -1 ? "red" : undefined}
