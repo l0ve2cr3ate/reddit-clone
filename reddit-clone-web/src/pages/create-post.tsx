9924a5d0dc937bd32d3ad70b2546af341f43b220
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-12-22 10:47:44 +0100

    refactor: Convert create post from urql to Apollo Client.

--- reddit-clone-web/src/pages/create-post.tsx
+++ reddit-clone-web/src/pages/create-post.tsx
@@ -1,27 +1,25 @@
 import { Box, Button, Heading } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
-import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import { FC } from "react";
 
 import InputField from "../components/InputField";
 import Layout from "../components/Layout";
 import { useCreatePostMutation } from "../generated/graphql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 import { useIsAuth } from "../utils/useIsAuth";
 
 export const CreatePost: FC = () => {
   const router = useRouter();
   useIsAuth();
-  const [, createPost] = useCreatePostMutation();
+  const [createPost] = useCreatePostMutation();
   return (
     <Layout variant="small">
       <Heading mb={5}>Create Post</Heading>
       <Formik
         initialValues={{ title: "", text: "" }}
         onSubmit={async (values) => {
-          const { error } = await createPost({ input: values });
-          if (!error) {
+          const { errors } = await createPost({ variables: { input: values } });
+          if (!errors) {
             router.push("/");
           }
         }}
@@ -53,4 +51,4 @@ export const CreatePost: FC = () => {
   );
 };
 
-export default withUrqlClient(createUrqlClient)(CreatePost);
+export default CreatePost;
